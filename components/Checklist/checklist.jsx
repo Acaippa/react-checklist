bb10b79be42ad17ad3a77ed12ec939e47809c1ee
Acaippa <olivernils8@gmail.com>
2026-02-23 07:45:00 +0100

    first commit

---
+++ components/Checklist/checklist.jsx
@@ -0,0 +1,71 @@
+function ChecklistItemCheckbox() {
+  return(
+    <input type="checkbox"/>
+  )
+}
+
+function CheckListDescription(){
+  return(
+    <p className="
+      text-[16px]
+    ">
+      This is really vital information
+    </p>
+  )
+}
+
+function ChecklistTitle(){
+  return(
+    <p
+    className="
+    text-[16px]
+    font-bold
+    ">Check me!</p>
+  )
+}
+
+function ChecklistItem(){
+  return(
+    <div className="
+    checklist-item 
+    py-[15px]
+    px-[25px] 
+    outline 
+    outline-solid 
+    outline-[#39383D] 
+    rounded-[10px]
+    w-125
+    bg-[#242328]
+    flex
+    flex-row
+    justify-between
+    items-center
+    ">
+      <div className="
+      flex
+      flex-col
+      items-start
+      ">
+        <ChecklistTitle/>
+        <CheckListDescription/>
+      </div>
+      <ChecklistItemCheckbox/>
+    </div>
+  )
+}
+
+export default function Checklist(){
+  return(
+    <div className="checklist-wrapper">
+      <div className="
+      checklist
+      flex 
+      flex-col
+      gap-6
+      ">
+        <ChecklistItem />
+        <ChecklistItem />
+      </div>
+    </div>
+  )
+}
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Checklist from '../components/Checklist/checklist'
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <p className="self-start font-bold text-[32px]">Todays tasks</p>
+    <Checklist />
+    </>
+  )
+}
+
+export default App
